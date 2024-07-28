d73f0068a468b32758eb7fe0a80dfa0335bd8310
karimaltohamy <karimbadr2003@gmail.com>
2024-07-28 15:03:23 +0300

    fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import FeaturedWork from "./components/featuredWork/FeaturedWork.component";
 import Headingsection from "./components/headingSection/Headingsection.component";
 import HeroSection from "./components/heroSection/HeroSection.compoent";
 import MySkills from "./components/mySkills/MySkills.component";
-import ContactUs from "./components/contactUs/ContactUs.component";
 
 export default function Home() {
   return (
